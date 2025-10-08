ba03cb599404644e5c899ecf471d6f8704c29c6e
Fuhu Xia <fxia@reisystems.com>
2025-10-07 23:49:01 -0400

    footer

---
+++ app/filters.py
@@ -0,0 +1,18 @@
+"""Jinja template filters for the catalog application."""
+
+from flask import url_for
+
+
+def usa_icon(icon_name: str) -> str:
+    """Return SVG markup for a USWDS icon referenced from the sprite sheet."""
+
+    sprite_path = url_for("static", filename="assets/uswds/img/sprite.svg")
+    return (
+        '<svg class="usa-icon" aria-hidden="true" role="img">'
+        f"<use xlink:href=\"{sprite_path}#{icon_name}\"></use>"
+        "</svg>"
+    )
+
+
+__all__ = ["usa_icon"]
+
