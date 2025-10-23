86bcee814b11c8246e2bc59f24801e55fe45e086
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2025-10-23 13:51:17 -0500

    Index more fields into OpenSearch

--- tests/test_opensearch.py
+++ tests/test_opensearch.py
@@ -21,3 +21,10 @@ class TestOpenSearch:
         # the test dataset has title "test"
         results = opensearch_client.search("test")
         assert len(results) > 0
+
+    def test_index_and_search_other_fields(self, interface_with_dataset, opensearch_client):
+        dataset_iterator = interface_with_dataset.db.query(Dataset)
+        opensearch_client.index_datasets(dataset_iterator)
+        # One of the Americorps datasets has tnxs-meph in an identifier
+        results = opensearch_client.search("tnxs-meph")
+        assert len(results) > 0
