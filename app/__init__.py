fd1639f7e26bfb71aa30e36500d074f4a3620fba
Fuhu Xia <fxia@reisystems.com>
2026-01-23 09:15:19 -0500

    Revert "CSP redux, no need to force HTTPS in Flask-talisman"
    This reverts commit 5f829d7950fa5946bf4642bc89b8f08def428a18.

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_htmx import HTMX
-from flask_talisman import Talisman
 
 from .filters import (
     fa_icon_from_extension,
@@ -65,54 +64,6 @@ def create_app(config_name: str = "local") -> Flask:
     app.add_template_filter(is_geometry_mapping)
     app.add_template_filter(geometry_to_mapping)
     app.add_template_filter(remove_html_tags)
-
-    # Content-Security-Policy headers
-    # single quotes need to appear in some of the strings
-    csp = {
-        "default-src": "'self'",
-        "script-src": " ".join([
-            "'self'",
-            "https://www.googletagmanager.com",
-            "https://buttons.github.io",  # github button
-            "https://touchpoints.app.cloud.gov",
-            "https://unpkg.com",  # leaflet
-            "'unsafe-hashes'",
-            "'sha256-osjxnKEPL/pQJbFk1dKsF7PYFmTyMWGmVSiL9inhxJY='",  # form autosubmit
-            "'sha256-A1KDZ6CTgI16YJ4cUNyyCFExM5+Sv4ApvahuZIQRXPA='",  # return to top
-        ]),
-        "font-src": " ".join([
-            "'self'",  # USWDS fonts
-            "https://cdnjs.cloudflare.com",  # font awesome
-        ]),
-        "img-src": " ".join([
-            "'self'",
-            "https://s3-us-gov-west-1.amazonaws.com",  # logos
-            "https://*.tile.openstreetmap.org",  # map tiles
-            "https://cg-1b082c1b-3db7-477f-9ca5-bd51a786b41e.s3-us-gov-west-1.amazonaws.com", # touchpoints
-            "https://touchpoints.app.cloud.gov",  # touchpoints
-        ]),
-        "connect-src": " ".join([
-            "'self'",
-            "https://api.github.com",
-        ]),
-        "frame-src": "https://www.googletagmanager.com",
-        "style-src-attr": " ".join([
-            "'self'", "'unsafe-hashes'",
-            "'sha256-kELgoK46JmGjLd8UHfzN0qJToDgIB+yMtRHG8PtGL7s='",  # Google tag manager inline
-        ]),
-        "style-src-elem": " ".join([
-            "'self'",  # local styles.css
-            "https://cdnjs.cloudflare.com",  # font-awesome 
-            "https://unpkg.com",  # leaflet
-            "'sha256-faU7yAF8NxuMTNEwVmBz+VcYeIoBQ2EMHW3WaVxCvnk='",  # htms.min.js
-            "'sha256-qo7STIM1L/OgU9y0De47mqod1UZFLJfTn36bRC42rfA='",  # buttons.js
-            "'sha256-d0LwTCBHt5DXTdSVbRSm0wQ/W4m5yoyMcrge+KrScUc='",  # touchpoints
-        ]),
-    }
-    Talisman(app, content_security_policy=csp,
-             content_security_policy_nonce_in=['script-src'],
-             # our https connections are terminated outside this app
-             force_https=False)
 
     return app
 
