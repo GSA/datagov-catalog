d8e75889c508109f7aea64976b10bdb9cc7241c2
Fuhu Xia <fxia@reisystems.com>
2026-02-03 12:13:27 -0500

    scroll also apply to text input

--- app/static/js/geography_autocomplete.js
+++ app/static/js/geography_autocomplete.js
@@ -676,6 +676,7 @@ class GeographyAutocomplete {
         this.selectedGeometry = JSON.parse(data.geometry);
         this.showClearButton();
         this.displayGeometry(this.selectedGeometry);
+        this.markPostApplyScroll();
         requestFilterFormSubmit(this.form);
       } catch (error) {
         console.error('Error loading location data:', error);
