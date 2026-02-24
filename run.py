405f9edc459d91bfb162abe74273a2cf1e9b89b4
Fuhu Xia <fxia@reisystems.com>
2026-02-24 09:35:25 -0500

    create_app(production) for https in url_for()

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
+
 from app import create_app
 
-app = create_app()
+# cloud.gov sets VCAP_APPLICATION; "production" gives https in url_for()
+app = create_app(
+    config_name="production" if os.getenv("VCAP_APPLICATION") else "local"
+)
 
 if __name__ == "__main__":
     app.run(debug=False, port=8080)
